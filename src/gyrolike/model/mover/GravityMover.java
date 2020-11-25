381e22e184ed6337842f7a9f8f21daa771506534
_ara0n_ <thomas99.montero@gmail.com>
2020-11-25 19:09:14 +0100

    added GravityMover

---
+++ src/gyrolike/model/mover/GravityMover.java
@@ -0,0 +1,26 @@
+package gyrolike.model.mover;
+
+import gyrolike.model.*;
+import gyrolike.model.sprite.Sprite;
+
+public class GravityMover extends SpriteMover {
+    private static final double G = 0.1;
+
+    public GravityMover(Sprite sprite) {
+        super(sprite);
+    }
+
+
+    @Override
+    public void move(Game gamaStatus) {
+        BoundingBox spriteBox = this.sprite.getBoundingBox(gamaStatus.getGrid().getSpritePos(sprite));
+
+        DiscretePosition leftCheck = new DiscretePosition((int) spriteBox.getBottomLeft().getX(), (int) (spriteBox.getBottomLeft().getY() - G));
+        DiscretePosition rightCheck = new DiscretePosition((int) spriteBox.getBottomRight().getX(), (int) (spriteBox.getBottomRight().getY() - G));
+        if (gamaStatus.getGrid().getTileAt(leftCheck) == Tile.AIR || gamaStatus.getGrid().getTileAt(rightCheck) == Tile.AIR) {
+            gamaStatus.getGrid().setSpritePos(this.sprite, new ContinuousPosition(spriteBox.getX(), spriteBox.getY() - G));
+        } else {
+            gamaStatus.getGrid().setSpritePos(this.sprite, new ContinuousPosition(spriteBox.getX(), (int) spriteBox.getY()));
+        }
+    }
+}
