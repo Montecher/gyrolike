ae9b6ce335f7ee5bd0240e1085511e25a908e810
_ara0n_ <thomas99.montero@gmail.com>
2020-11-25 15:35:40 +0100

    moved to better package

--- src/gyrolike/model/Tile.java
+++ src/gyrolike/model/Tile.java
@@ -1,4 +1,4 @@
-package model;
+package gyrolike.model;
 
 public enum Tile {
     ROPE (false, true, false),
@@ -8,26 +8,14 @@ public enum Tile {
     COLUMN (true, false, true),
     PLATFORM(true, false, true);
 
-    private final boolean solid;
-    private final boolean rope;
-    private final boolean dynamic;
+    public final boolean solid;
+    public final boolean rope;
+    public final boolean dynamic;
 
     Tile(boolean solid, boolean rope, boolean dynamic) {
         this.solid = solid;
         this.rope = rope;
         this.dynamic = dynamic;
-    }
-
-    boolean isSolid() {
-        return solid;
-    }
-
-    boolean isRope() {
-        return rope;
-    }
-
-    boolean isDynamic() {
-        return dynamic;
     }
 }
 
