b1b7c003d3175ae3e6a3fd7db040edf070c927b7
_ara0n_ <thomas99.montero@gmail.com>
2020-12-08 20:29:15 +0100

    column is alive

--- src/gyrolike/model/mover/ColumnMover.java
+++ src/gyrolike/model/mover/ColumnMover.java
@@ -5,7 +5,7 @@ import gyrolike.model.Game;
 import gyrolike.model.Tile;
 
 public class ColumnMover extends TileMover {
-    private final int x, y, height;
+    private int x, y, height;
 
     public ColumnMover(int x, int y, int height) {
         this.x = x;
@@ -15,25 +15,29 @@ public class ColumnMover extends TileMover {
 
     @Override
     public void move(Game gameStatus) {
-        switch (gameStatus.getColumnDirection()) {
-            case UP:
-                if (y + height + 1 < gameStatus.getGrid().getWidth() && gameStatus.getGrid().getTileAt(new DiscretePosition(x, y + height + 1)) == Tile.AIR) {
-                    for (int i = height; i > 0; i--) {
-                        gameStatus.getGrid().setTileAt(new DiscretePosition(x, i), gameStatus.getSelectedColor());
+        if (gameStatus.getSelectedColor() == gameStatus.getGrid().getTileAt(x, y)) {
+            switch (gameStatus.getColumnDirection()) {
+                case UP:
+                    if (y + height < gameStatus.getGrid().getHeight() && gameStatus.getGrid().getTileAt(new DiscretePosition(x, y + height)) == Tile.AIR) {
+                        for (int i = height; i > 0; i--) {
+                            gameStatus.getGrid().setTileAt(new DiscretePosition(x, y + i), gameStatus.getSelectedColor());
+                        }
+                        gameStatus.getGrid().setTileAt(new DiscretePosition(x, y), Tile.AIR);
+                        this.y++;
                     }
-                    gameStatus.getGrid().setTileAt(new DiscretePosition(x, y), Tile.AIR);
-                }
-                break;
-            case DOWN:
-                if (0 < y - 1 && gameStatus.getGrid().getTileAt(new DiscretePosition(x, y - 1)) == Tile.AIR) {
-                    for (int i = 0; i < height; i++) {
-                        gameStatus.getGrid().setTileAt(new DiscretePosition(x, y + i - 1), gameStatus.getSelectedColor());
+                    break;
+                case DOWN:
+                    if (0 < y && gameStatus.getGrid().getTileAt(new DiscretePosition(x, y - 1)) == Tile.AIR) {
+                        for (int i = 0; i < height; i++) {
+                            gameStatus.getGrid().setTileAt(new DiscretePosition(x, y + i - 1), gameStatus.getSelectedColor());
+                        }
+                        gameStatus.getGrid().setTileAt(new DiscretePosition(x, y + height - 1), Tile.AIR);
+                        this.y--;
                     }
-                    gameStatus.getGrid().setTileAt(new DiscretePosition(x, y + height), Tile.AIR);
-                }
-                break;
-            default:
-                break;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
